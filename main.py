cf16817497a1dd0f874a838e58e5da4b3398a04c
mr-prantik <prantikmahata9211@gmail.com>
2023-02-22 02:17:50 +0530

    updated

--- main.py
+++ main.py
@@ -1,2 +1,49 @@
-import pygame
-pygame.mixer.init()
+#sample code using the PyQt5 framework to play an MP3 file:
+
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QSlider, QAction
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+
+class MusicPlayer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize the music player
+        self.player = QMediaPlayer()
+
+        # Add the slider for volume control
+        self.volume_slider = QSlider(Qt.Horizontal)
+        self.volume_slider.setMinimum(0)
+        self.volume_slider.setMaximum(100)
+        self.volume_slider.setFixedWidth(100)
+        self.volume_slider.setValue(self.player.volume())
+        self.volume_slider.valueChanged.connect(self.player.setVolume)
+
+        # Add the "Open" action to the file menu
+        open_action = QAction('Open', self)
+        open_action.triggered.connect(self.open_file)
+
+        # Create the menu bar
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('File')
+        file_menu.addAction(open_action)
+
+        # Add the volume slider to the status bar
+        self.statusBar().addPermanentWidget(self.volume_slider)
+
+        # Set the main window properties
+        self.setWindowTitle('Music Player')
+        self.setGeometry(100, 100, 600, 400)
+
+    def open_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Open file', '', 'Music Files (*.mp3)')
+        if file_path != '':
+            self.player.setMedia(QMediaContent(file_path))
+            self.player.play()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    player = MusicPlayer()
+    player.show()
+    sys.exit(app.exec_())
